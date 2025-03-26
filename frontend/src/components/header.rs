551ffdfb2a518a0598b10859edd7e2231ee0ec81
Fernando Ferreira <fm_ferreira1996@hotmail.com>
2025-03-26 18:23:33 +0000

    Add app title to header

--- frontend/src/components/header.rs
+++ frontend/src/components/header.rs
@@ -10,9 +10,10 @@ pub fn header() -> Html {
     html! {
         <header class="sticky top-0 z-50">
             <div class="navbar bg-primary text-primary-content">
-                <div class="flex-1">
+                <div class="flex">
                     <Logo />
                 </div>
+                <div class="ml-4 flex-1 text-lg font-bold">{"Deck Manager"}</div>
                 <div class="flex gap-2">
                     <NavLink label={"Decks"} route={Route::Decks} />
                     <NavLink label={"Packages"} route={Route::Packages} />
